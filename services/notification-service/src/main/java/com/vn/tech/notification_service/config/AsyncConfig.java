6e55f1463cf94cb5e4180132286d662fcd04d90f
long <phamthanhlong725@gmail.com>
2026-04-09 19:46:51 +0700

    notification-service api done send email

---
+++ services/notification-service/src/main/java/com/vn/tech/notification_service/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.vn.tech.notification_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync // BẬT CÁI NÀY LÊN THÌ @Async MỚI CÓ TÁC DỤNG
+public class AsyncConfig {
+}
