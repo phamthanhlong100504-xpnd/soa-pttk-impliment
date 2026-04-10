554b34a0b63b8e8dd5efd47b7589a2302c407df7
long <phamthanhlong725@gmail.com>
2026-04-10 10:54:37 +0700

    test communication success

--- services/inventory-service/src/main/java/com/vn/tech/inventory_service/InventoryManagementServiceApplication.java
+++ services/inventory-service/src/main/java/com/vn/tech/inventory_service/InventoryManagementServiceApplication.java
@@ -3,11 +3,15 @@ package com.vn.tech.inventory_service;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class InventoryManagementServiceApplication {
 
     @PostConstruct
